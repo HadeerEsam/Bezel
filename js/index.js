859dbb08c6e123a2f9bce3022ea3ee3f619ac598
Hadeer Esam <hadeeresamcom33@gmail.com>
2020-08-10 01:51:25 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ let aboutOffset= $("#about").offset().top;
 
     //slide up to home
 $("#btnUp").click(function () { 
-    $("html,body").animate({scrollTop:'0'},3000);
+    $("html,body").animate({scrollTop:'0'},700);
 });
 
 //slide to certain section 
